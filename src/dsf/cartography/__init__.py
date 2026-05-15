42680231b3ba6460b4d7ad4e12e036d3382fa8f8
Gregorio Berselli <gregorio.berselli@studio.unibo.it>
2026-05-15 11:42:46 +0200

    Split `get_cartography` in subfunction and add SCC filter (#471)
    * Split `get_cartography` in subfunction and add SCC filter
    
    * Formatting

--- src/dsf/cartography/__init__.py
+++ src/dsf/cartography/__init__.py
@@ -1,7 +1,19 @@
 from .cartography import (
-    get_cartography as get_cartography,
-    graph_from_gdfs as graph_from_gdfs,
-    graph_to_gdfs as graph_to_gdfs,
-    create_manhattan_cartography as create_manhattan_cartography,
-    to_folium_map as to_folium_map,
+    fetch_cartography,
+    process_cartography,
+    get_cartography,
+    graph_from_gdfs,
+    graph_to_gdfs,
+    create_manhattan_cartography,
+    to_folium_map,
 )
+
+__all__ = [
+    "fetch_cartography",
+    "process_cartography",
+    "get_cartography",
+    "graph_from_gdfs",
+    "graph_to_gdfs",
+    "create_manhattan_cartography",
+    "to_folium_map",
+]
