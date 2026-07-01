0ba639a8030a7d0968252467ad4986a5e1974c07
Grufoony <gregorio.berselli@studio.unibo.it>
2026-07-01 11:00:52 +0200

    Add format compatibility for PathCollection

--- src/dsf/base/PathCollection.hpp
+++ src/dsf/base/PathCollection.hpp
@@ -2,6 +2,7 @@
 
 #include "../utility/Typedef.hpp"
 
+#include <format>
 #include <list>
 #include <unordered_map>
 #include <vector>
@@ -18,3 +19,26 @@ namespace dsf {
     std::list<std::vector<Id>> explode(Id const sourceId, Id const targetId) const;
   };
 }  // namespace dsf
+
+template <>
+struct std::formatter<dsf::PathCollection> : std::formatter<std::string> {
+  constexpr auto parse(std::format_parse_context& ctx) {
+    return std::formatter<std::string>::parse(ctx);
+  }
+  auto format(const dsf::PathCollection& pathCollection, std::format_context& ctx) const {
+    std::string result = "{\n";
+    for (const auto& [key, value] : pathCollection) {
+      result += std::format("  {}: [", key);
+      for (const auto& id : value) {
+        result += std::format("{}, ", id);
+      }
+      if (!value.empty()) {
+        result.pop_back();
+        result.pop_back();
+      }
+      result += "],\n";
+    }
+    result += "}";
+    return std::formatter<std::string>::format(result, ctx);
+  }
+};
